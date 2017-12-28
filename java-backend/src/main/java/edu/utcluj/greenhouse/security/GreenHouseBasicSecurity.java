161f118fb99db97a761c579dcb451d61c85effbc
RazvanSebastian <rzvs95@gmail.com>
2017-12-28 13:40:35 +0200

    Backend Basic Authentication Configuration #11

---
+++ java-backend/src/main/java/edu/utcluj/greenhouse/security/GreenHouseBasicSecurity.java
@@ -0,0 +1,37 @@
+package edu.utcluj.greenhouse.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * 
+ * @author Razvan
+ *
+ * Only one user has the authority to access resources => user : greenhouse and password : strongpassword
+ * To be able to access end-points the user must send an additional header:
+ * Key : Authorization and Value : greenhouse:strongpassword (encoded to base64)
+ */
+
+@Configuration
+@EnableWebSecurity
+public class GreenHouseBasicSecurity extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("greenhouse").password("strongpassword").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().httpBasic();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	
+	
+}
